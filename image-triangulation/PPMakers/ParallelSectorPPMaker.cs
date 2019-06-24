c732c9c7ce4ad44a6ac1e52edf14cb5016440282
Lobarev A.A <andrewlobarev.mail@gmail.com>
2019-06-24 21:08:04 +0300

    Working on ParallelSectorPPMaker.cs.

---
+++ image-triangulation/PPMakers/ParallelSectorPPMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace image_triangulation
+{
+    class ParallelSectorPPMaker
+    {
+        public static void Run(Bitmap sourceImage, HashSet<Pixel> pivotPoints, byte threshold, int sectorSize)
+        {
+            // Блокируем в памяти Bitmap исходного изображения
+            Rectangle rect = new Rectangle(0, 0, sourceImage.Width, sourceImage.Height);
+            System.Drawing.Imaging.BitmapData bmpData = sourceImage.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                                                                             sourceImage.PixelFormat);
+            // Получаем адрес первой строки
+            IntPtr ptr = bmpData.Scan0;
+            // Сохраняем байты в массив
+            int numberOfBytes = Math.Abs(bmpData.Stride) * bmpData.Height;
+            byte[] rgbValues = new byte[numberOfBytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, numberOfBytes);
+        }
+    }
+}
