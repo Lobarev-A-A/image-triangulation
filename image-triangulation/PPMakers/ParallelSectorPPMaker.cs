531ad79fff22d0a4d8e7a776291cd51be43106eb
Lobarev A.A <andrewlobarev.mail@gmail.com>
2019-06-24 23:34:56 +0300

    Working on ParallelSectorPPMaker.cs.

--- image-triangulation/PPMakers/ParallelSectorPPMaker.cs
+++ image-triangulation/PPMakers/ParallelSectorPPMaker.cs
@@ -5,11 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace image_triangulation
 {
     class ParallelSectorPPMaker
     {
-        public static void Run(Bitmap sourceImage, HashSet<Pixel> pivotPoints, byte threshold, int sectorSize)
+        private Bitmap sourceImage;
+        private HashSet<Pixel> pivotPoints;
+        private byte threshold;
+        private int sectorSize;
+        private int width;
+        private int height;
+
+        public ParallelSectorPPMaker(Bitmap sourceImage, HashSet<Pixel> pivotPoints, byte threshold, int sectorSize)
+        {
+            this.sourceImage = sourceImage;
+            this.pivotPoints = pivotPoints;
+            this.threshold = threshold;
+            this.sectorSize = sectorSize;
+            this.width = sourceImage.Width;
+            this.height = sourceImage.Height;
+        }
+
+        public void Run()
         {
             // Блокируем в памяти Bitmap исходного изображения
             Rectangle rect = new Rectangle(0, 0, sourceImage.Width, sourceImage.Height);
@@ -21,6 +39,13 @@ namespace image_triangulation
             int numberOfBytes = Math.Abs(bmpData.Stride) * bmpData.Height;
             byte[] rgbValues = new byte[numberOfBytes];
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, numberOfBytes);
+
+
+        }
+
+        int GetIndex(int x, int y)
+        {
+            return y * width + x;
         }
     }
 }
