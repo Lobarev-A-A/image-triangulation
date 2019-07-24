bda63e5a3c40ba710ff4e1c63574908455593e03
Lobarev A.A <andrewlobarev.mail@gmail.com>
2019-07-24 12:07:31 +0300

    Added unit testing project.

---
+++ image-triangulation.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace image_triangulation.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
